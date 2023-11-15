7708998ac142280525368c15d8509dd70d3ed42e
akterCloudPro <73134659+akterCloudPro@users.noreply.github.com>
2023-11-15 14:36:32 +0600

    Create DetectionScriptForCrowdStrikeService.ps1

---
+++ scripts/remediation/DetectionScriptForCrowdStrikeService.ps1
@@ -0,0 +1,30 @@
+## Detection script for CrowdStrike Service
+## Author: 
+## Date:  
+
+# Define the service name
+$serviceName = "CrowdStrike Falcon Sensor Service"
+
+# Check if the service exists
+if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
+    # Service exists, check if it is running
+    $serviceStatus = (Get-Service -Name $serviceName).Status
+    
+    if ($serviceStatus -eq 'Running') {
+        Write-Host "$serviceName is running."
+        # Return Exit Code 0 (success)
+        exit 0
+    }
+    else {
+        Write-Host "$serviceName is not running."
+        # Return Exit Code 1 (failure)
+        exit 1
+        Break
+    }
+}
+else {
+    Write-Host "$serviceName does not exist."
+    # Return Exit Code 1 (failure)
+    exit 1
+    Break
+}
