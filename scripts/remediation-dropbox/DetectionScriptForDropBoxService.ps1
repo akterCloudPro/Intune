33991adc7d93fff287f443bdf777f2edc30e4ef0
akterCloudPro <73134659+akterCloudPro@users.noreply.github.com>
2023-11-22 01:30:55 +0600

    Script Created

---
+++ scripts/remediation-dropbox/DetectionScriptForDropBoxService.ps1
@@ -0,0 +1,30 @@
+## Detection script for DropBox Service
+## Author: Akter Hossain
+## Date: 15/11/2023
+
+# Define the service name
+$serviceName = "DbxSvc"
+
+# Check if the service exists
+if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
+    # Service exists, check if it is running
+    $serviceStatus = (Get-Service -Name $serviceName).Status
+    
+    if ($serviceStatus -eq 'Running') {
+        Write-Host "$serviceName is running."
+        # Return Exit Code 0 (success)
+        exit 0
+    }
+    else {
+        Write-Host "$serviceName is not running."
+        # Return Exit Code 1 (failure)
+        exit 1
+        Break
+    }
+}
+else {
+    Write-Host "$serviceName does not exist."
+    # Return Exit Code 1 (failure)
+    exit 1
+    Break
+}
